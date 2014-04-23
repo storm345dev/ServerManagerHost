0582b2ab957ffc404b2adb6235669f797617be3d
storm345 <storm345dev@gmail.com>
2014-04-23 21:01:42 +0100

    Increase msg limit

--- ServerManager/src/net/stormdev/MTA/SM/connections/Message.java
+++ ServerManager/src/net/stormdev/MTA/SM/connections/Message.java
@@ -9,7 +9,7 @@ import net.stormdev.MTA.SM.core.Core;
 
 public class Message {
 	
-	private static final int msgSplineLength = 256;
+	private static final int msgSplineLength = 5000;
 	private String to;
 	private String from;
 	private String msgTitle;
